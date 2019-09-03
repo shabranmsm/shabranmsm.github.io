5ba3680f71a1737ebb61f48e30c80a5e989cdd77
shabranmsm <shabranmetafarady@gmail.com>
2019-09-03 23:22:54 +0700

    first commit

--- common-js/scripts.js
+++ common-js/scripts.js
@@ -2,20 +2,20 @@
 (function ($) {
 
     "use strict";
-	
-	
+
+
 
 	// LINE PROGRESS BAR
 	enableLineProgress();
-	
+
 	// RADIAL PROGRESS BAR
 	enableRadialProgress();
-	
+
 	// ACCORDIAN
 	panelAccordian();
 
 	$(window).on('load', function(){
-		
+
 		// ISOTOPE PORTFOLIO WITH FILTER
 		if(isExists('.portfolioContainer')){
 			var $container = $('.portfolioContainer');
@@ -27,11 +27,11 @@
 					queue: false
 				}
 			});
-		 
+
 			$('.portfolioFilter a').click(function(){
 				$('.portfolioFilter .current').removeClass('current');
 				$(this).addClass('current');
-		 
+
 				var selector = $(this).attr('data-filter');
 				$container.isotope({
 					filter: selector,
@@ -42,53 +42,53 @@
 					}
 				 });
 				 return false;
-			}); 
+			});
 		}
-	
+
 	});
-	
-	
+
+
 	$('a[href="#"]').on('click', function(event){
 		return;
 	});
-	
-	
+
+
 	if ( $.isFunction($.fn.fluidbox) ) {
 		$('a').fluidbox();
 	}
-	
+
 	var countCounterUp = 0;
-	
+
 	var a = 0 ;
-	
+
 	countCounterUp = enableCounterUp(countCounterUp);
-	
+
 	$(window).on('scroll', function(){
-		
+
 		countCounterUp = enableCounterUp(countCounterUp);
-	
+
 	});
-	
-	
+
+
 })(jQuery);
 
 function panelAccordian(){
-	
+
 	var panelTitle = $('.panel-title');
 	panelTitle.on('click', function(){
 		$('.panel-title').removeClass('active');
 		$(this).toggleClass('active');
-		
+
 	});
-	
+
 }
 
 function enableRadialProgress(){
-	
+
 	$(".radial-progress").each(function(){
 		var $this = $(this),
 			progPercent = $this.data('prog-percent');
-			
+
 		var bar = new ProgressBar.Circle(this, {
 			color: '#aaa',
 			strokeWidth: 3,
@@ -96,10 +96,10 @@ function enableRadialProgress(){
 			easing: 'easeInOut',
 			duration: 1400,
 			text: {
-				
+
 			},
 			from: { color: '#aaa', width: 1 },
-			to: { color: '#FEAE01', width: 3 },
+			to: { color: '#000000', width: 3 },
 			// Set default step function for all animate calls
 			step: function(state, circle) {
 				circle.path.setAttribute('stroke', state.color);
@@ -114,54 +114,54 @@ function enableRadialProgress(){
 
 			}
 		});
-		
+
 		$(this).waypoint(function(){
-		   bar.animate(progPercent);  
+		   bar.animate(progPercent);
 		},{offset: "90%"})
-		
+
 	});
 }
 
 function enableLineProgress(){
-	
+
 	$(".line-progress").each(function(){
 		var $this = $(this),
 			progPercent = $this.data('prog-percent');
-			
+
 		var bar = new ProgressBar.Line(this, {
 			strokeWidth: 1,
 			easing: 'easeInOut',
 			duration: 1400,
-			color: '#FEAE01',
+			color: '#000000',
 			trailColor: '#eee',
 			trailWidth: 1,
 			svgStyle: {width: '100%', height: '100%'},
 			text: {
 				style: {
-					
+
 				},
 			},
-			from: {color: '#FFEA82'},
+			from: {color: '#000000'},
 			to: {color: '#ED6A5A'},
 			step: (state, bar) => {
 				bar.setText(Math.round(bar.value() * 100) + ' %');
 			}
 		});
-		
+
 		$(this).waypoint(function(){
-		   bar.animate(progPercent);  
+		   bar.animate(progPercent);
 		},{offset: "90%"})
-		
+
 	});
 }
 
 function enableCounterUp(a){
-	
+
 	var counterElement;
-	
+
 	if(isExists('#counter')){ counterElement = $('#counter'); }
 	else{ return; }
-		
+
 	var oTop = $('#counter').offset().top - window.innerHeight;
 	if (a == 0 && $(window).scrollTop() > oTop) {
 		$('.counter-value').each(function() {
@@ -192,7 +192,7 @@ function enableCounterUp(a){
 }
 
 function isExists(elem){
-	if ($(elem).length > 0) { 
+	if ($(elem).length > 0) {
 		return true;
 	}
 	return false;
